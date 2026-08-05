12069e303408347397241d52afc2ac8ba5163ede
aryangupta2048 <aryangupta2048@gmail.com>
2026-08-05 10:26:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.schema import Document
 
 
 # ----------------------------- Page Config ----------------------------- #
